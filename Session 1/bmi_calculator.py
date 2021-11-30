6eeea9c350ed2b182f6a091a4067b232aa1d2afd
Ehsan Hosseini <esi.fury11@gmail.com>
2021-11-30 22:25:08 +0330

    Session 1 Assignments

--- Session 1/bmi_calculator.py
+++ Session 1/bmi_calculator.py
@@ -3,6 +3,7 @@ weight = float(input('vazne khod ra be kg vared konid: '))
 
 bmi = weight / (height ** 2)
 print('{:.2f}'.format(bmi), end=' ')
+
 if bmi > 0 and bmi <=18.5:
   print('Underweight')
 elif bmi < 25:
